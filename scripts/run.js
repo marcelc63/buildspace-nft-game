f54bf0d2025992b5a9d624ea3144e241a1714fce
Marcel <marcelc6363@gmail.com>
2021-10-24 10:50:26 +0700

    Create script

--- scripts/run.js
+++ scripts/run.js
@@ -1,25 +1,9 @@
 const main = async () => {
-  const ContractFactory = await hre.ethers.getContractFactory(
-    "CoolCatsTemplate"
-  );
+  const ContractFactory = await hre.ethers.getContractFactory("NFTGame");
 
-  // TODO: Change to where you will host your metadata
-  const baseURI = "https://api.example.com";
-
-  const contract = await ContractFactory.deploy({ args: [baseURI] });
+  const contract = await ContractFactory.deploy();
   await contract.deployed();
   console.log("Contract deployed to:", contract.address);
-
-  // Activate minting
-  let activateTxn = await contract.pause(false);
-  await activateTxn.wait();
-
-  // Call the function.
-  let adoptTxn = await contract.adopt(20, {
-    value: hre.ethers.utils.parseEther("1.2"),
-  });
-  // Wait for it to be mined.
-  await adoptTxn.wait();
 };
 
 const runMain = async () => {
